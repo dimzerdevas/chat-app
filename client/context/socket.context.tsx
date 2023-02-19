a11aeadb8916e45d5b7a5920e1ed7a9e85e63576
Dimitris Zerdevas <42102327+dimzerdevas@users.noreply.github.com>
2023-02-19 18:10:27 +0200

    init commit

---
+++ client/context/socket.context.tsx
@@ -0,0 +1,28 @@
+import io, { Socket } from 'socket.io-client';
+import { createContext, useContext, useState } from 'react';
+import { SOCKET_URL } from '../config/default';
+
+interface Context {
+    socket: Socket;
+    username?: string;
+    setUsername?: Function;
+}
+
+const socket = io(SOCKET_URL);
+
+const SocketContext = createContext<Context>({socket, setUsername: () => false});
+
+function SocketsProvider(props: any) {
+    const [username, setUsername] = useState("");
+
+    return (
+        <SocketContext.Provider
+            {...props}
+            value={{ socket, username, setUsername }}
+        />
+    )
+}
+
+export const useSockets = () => useContext(SocketContext);
+
+export default SocketsProvider;
---
+++ client/pages/index.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { useSockets } from '../context/socket.context'
+
+import RoomsContainer from '../containers/Rooms';
+import MessagesContainer from '../containers/Messages';
+import { useRef } from 'react';
+
+export default function Home() {
+  const { socket, username, setUsername } = useSockets();
+  const usernameRef = useRef(null);
+
+  function handleSetUsername() {
+    const value = usernameRef.current.value;
+    if (!value) {
+      return;
+    }
+    setUsername(value);
+    localStorage.setItem("username", value)
+  }
+
+  return (
+    <div>
+      {username
+        ? (
+          <div className={styles.usernameWrapper}>
+            <input placeholder='username' ref={usernameRef} />
+            <button onClick={handleSetUsername}>START</button>
+          </div>)
+        : (
+          <>
+            <RoomsContainer />
+            <MessagesContainer />
+          </>)
+      }
+    </div>
+  )
+}
